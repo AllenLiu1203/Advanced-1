b0da44c40fafd51041f7d6d0696649bf726ba96c
AllenLiu1203 <48576577+AllenLiu1203@users.noreply.github.com>
2019-04-26 02:44:24 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  A1
+//
+//  Created by 劉世翔 on 2019/4/26.
+//  Copyright © 2019年 劉世翔. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char n;
+    for (n=65; n<=90; n++)
+    {
+        printf("%c ",n);
+    }
+    
+    printf("\n");
+    for (n=97; n<=122; n++)
+    {
+        printf("%c ",n);
+    }
+    printf("\n");
+    system ("pause");
+    return 0;
+}
